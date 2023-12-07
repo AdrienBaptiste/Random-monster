9f9cd70da7d900e7deed99b22ddfbe71af4398a2
Adrien Baptiste <you@example.com>
2023-12-07 18:46:25 +0100

    reglage bug

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
 var id = 1;
-const imgContainer1 = document.querySelector(".container1 img");
-const imgContainer2 = document.querySelector(".container2 img");
-const imgContainer3 = document.querySelector(".container3 img");
-
 var srcImg1 = "image/monstre1";
 var srcImg2 = "image/monstre1";
 var srcImg3 = "image/monstre1";
-
+const imgContainer1 = document.querySelector(".container1 img");
+const imgContainer2 = document.querySelector(".container2 img");
+const imgContainer3 = document.querySelector(".container3 img");
 const button = document.querySelector(".button_container button");
 
-var lien = "images/monstre" + id + ".png";
-/*
-button.addEventListener('click', function() {
-    id = Math.floor(Math.random() * 2)
-    srcImg1 = "images/monstre" + id + ".png";
-    id = Math.floor(Math.random() * 2)
-    srcImg2 = "images/monstre" + id + ".png";
-    id = Math.floor(Math.random() * 2)
-    srcImg3 = "images/monstre" + id + ".png";
-    setTimeout(() => {
-        imgContainer1.src = srcImg1
-        imgContainer2.src = srcImg2
-        imgContainer3.src = srcImg3
-      }, 300);
-});
 
-*/
 function randomId () {
     id = Math.floor(Math.random() * 3);
 }
-
 
 function changeLink () {
     randomId();
@@ -41,6 +22,6 @@ function changeLink () {
 }
 
 button.addEventListener('click', function() {
-    changeLink(lien);
+    changeLink();
 });
 
