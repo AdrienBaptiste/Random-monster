dc28634109360555ae32581c0056250d76e07856
Adrien <120730244+AdrienBaptiste@users.noreply.github.com>
2023-12-12 00:04:51 +0100

    new monster

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var id = 1;
+var id1 = 1;
+var id2 = 1;
+var id3 = 1;
 var srcImg1 = "image/monstre1";
 var srcImg2 = "image/monstre1";
 var srcImg3 = "image/monstre1";
+const range = 7;
 const imgContainer1 = document.querySelector(".container1 img");
 const imgContainer2 = document.querySelector(".container2 img");
 const imgContainer3 = document.querySelector(".container3 img");
@@ -9,18 +12,27 @@ const generateBtn = document.querySelector(".button_container .genererBtn");
 const downloadBtn = document.querySelector(".button_container .telechargerBtn");
 
 
-function randomId () {
-    id = Math.floor(Math.random() * 6);
-    console.log(id);
+function randomId1 () {
+    id1 = Math.floor(Math.random() * range);
+}
+function randomId2 () {
+    do {
+        id2 = Math.floor(Math.random() * range);
+    } while (id2 === id1);
+}
+function randomId3 () {
+    do {
+        id3 = Math.floor(Math.random() * range);
+    } while (id3 === id2 || id3 === id1);
 }
 
 function changeLink () {
-    randomId();
-    imgContainer1.src = "images/monstre" + id + ".png";
-    randomId();
-    imgContainer2.src = "images/monstre" + id + ".png";
-    randomId();
-    imgContainer3.src = "images/monstre" + id + ".png";
+    randomId1();
+    imgContainer1.src = "images/monstre" + id1 + ".png";
+    randomId2();
+    imgContainer2.src = "images/monstre" + id2 + ".png";
+    randomId3();
+    imgContainer3.src = "images/monstre" + id3 + ".png";
 }
 
 generateBtn.addEventListener('click', function() {
@@ -38,4 +50,3 @@ downloadBtn.addEventListener("click", () => {
         anchor.remove();
     })
 });
-
